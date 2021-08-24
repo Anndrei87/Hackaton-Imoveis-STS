4d249de946eae3227bbeae74b6b7faafd12b1af9
Duan Camargo <duan1505@gmail.com>
2021-08-24 15:44:06 -0300

    Imovel Model

---
+++ src/main/java/com/darahimoveis/model/Imovel.java
@@ -0,0 +1,17 @@
+package com.darahimoveis.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "imovel")
+public class Imovel {
+    
+}

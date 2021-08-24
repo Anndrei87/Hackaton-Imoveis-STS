5489e22d8869b88401eb40b7a2b94ac1c75adc51
Duan Camargo <duan1505@gmail.com>
2021-08-24 14:00:44 -0300

    Frist Commit

---
+++ src/test/java/com/darahimoveis/application/DarahimoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darahimoveis.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarahimoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

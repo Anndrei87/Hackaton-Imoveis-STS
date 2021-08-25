e824fd37fab244d24141391f70411f99b6d69eae
Duan Camargo <duan1505@gmail.com>
2021-08-25 20:26:16 -0300

    troca_de_senha

--- src/main/java/com/darahimoveis/controller/UsuarioController.java
+++ src/main/java/com/darahimoveis/controller/UsuarioController.java
@@ -23,7 +23,7 @@ public class UsuarioController {
 		@RequestParam(required = true, defaultValue = "Camargo") String sobrenome,
 		@RequestParam(required = true, defaultValue = "+5571991801813") String whatsapp,
 		@RequestParam(required = true, defaultValue = "duan1505@gmail.com") String email,
-		@RequestParam(required = true, defaultValue = "12345") String senha)
+		@RequestParam(required = true, defaultValue = "123") String senha)
 		{
 
 		usuarioService.save(nome, sobrenome, whatsapp, email, senha);

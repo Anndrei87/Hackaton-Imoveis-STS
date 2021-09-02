912bb65ced41dd7e3949675e610a7d583e21336e
Duan Camargo <duan1505@gmail.com>
2021-09-02 16:27:45 -0300

    changes

--- src/main/java/com/darahimoveis/application/controller/ContasController.java
+++ src/main/java/com/darahimoveis/application/controller/ContasController.java
@@ -1,12 +1,30 @@
 package com.darahimoveis.application.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.darahimoveis.application.model.Contas;
+import com.darahimoveis.application.model.Usuario;
+import com.darahimoveis.application.service.ContasService;
 
 @RestController
 @RequestMapping("/contas")
 @CrossOrigin
 public class ContasController {
 
+	@Autowired
+	ContasService contasService;
+	
+	/*GetMappings*/
+	@GetMapping("/findById")
+	public Contas findByID(@RequestParam Integer id) {
+		Optional<Contas> contas =  contasService.findById(id);
+		return contas.get();
+	}
 }
--- src/main/java/com/darahimoveis/application/service/ContasService.java
+++ src/main/java/com/darahimoveis/application/service/ContasService.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.darahimoveis.application.model.Contas;
 
@@ -15,15 +13,14 @@ public class ContasService {
 	@Autowired
 	ContasService contasService;
     
-    @GetMapping("/findAll")
+	
 	public Optional<Contas> findAll() {
 		Optional<Contas> contas = contasService.findAll();
 		return contas;
 	}	
     
-    @GetMapping("/findById")
-	public Optional<Contas> findByID(@RequestParam Integer id) {
-		Optional<Contas> contas = contasService.findByID(id);
+    public Optional<Contas> findById(Integer id) {
+		Optional<Contas> contas = contasService.findById(id);
 		return contas;
 	}
 }
